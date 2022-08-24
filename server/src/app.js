7332f299ce3f17de23e707ed2d947945b8a4560e
Frost4359 <108847925+Frost4359@users.noreply.github.com>
2022-08-24 22:23:13 +0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+require('./routes')(app)
+
 app.get('/status', function (req, res){
     res.send('ควยยย')
 })
@@ -15,31 +17,8 @@ app.get('/hello/:person', function (req, res) {
     res.send('sey hello with ' + req.params.person)
 })
 
-// get user by id
-app.get('/user/:userId', function (req, res){
-    res.send('ดูข้อมูลผู้ใช้งาน:' + req.params.userId)
-})
-
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด:')
-})
-
-// create user
-app.put('/user/:uerId', function (req, res){
-    res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
-})
-
-// edit user
-app.put('/user/:userId', function (req, res){
-    res.send('ทำการแก้ไขผู้ใช้งาน:' + req.params.userId + ' : ' +
-    JSON.stringify(req, body))
-})
-
-// delete user
-app.delete('/user/:userId', function (req, res){
-    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
-    JSON.stringify(req.body))
+app.post('/hello', function (req,res){
+    res.send('OK you post - ' + req.body.name)
 })
 
 let port = 8081
